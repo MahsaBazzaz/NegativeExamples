8486defb709953bab2abf35a140ab82f1971c213
MahsaBazzaz <mahsa.bazzaz@gmail.com>
2024-06-28 12:12:29 -0400

    update generation script

--- generate.sh
+++ generate.sh
@@ -1,38 +1,55 @@
 #!/bin/bash
 
 # exp1 
-python ./vanilla/generate_exp1.py --game mario --instance 1 --epochs 500 --solution True
-python ./rumi/generate_exp1.py --game mario --instance 1 --epochs 500 --solution True
-python ./conditional/generate_exp1.py --game mario --instance 1 --epochs 500 --solution True
+python ./vanilla/generate_exp1.py --game mario --instance 1 --epochs 2000 --solution True
+python ./rumi/generate_exp1.py --game mario --instance 1 --epochs 1500 --solution True
+python ./conditional/generate_exp1.py --game mario --instance 1 --epochs 1000 --solution True
 
-python ./vanilla/generate_exp1.py --game cave_treasures --instance 1 --epochs 500 --solution True
-python ./rumi/generate_exp1.py --game cave_treasures --instance 1 --epochs 500 --solution True
-python ./conditional/generate_exp1.py --game cave_treasures --instance 1 --epochs 500 --solution True
+python ./vanilla/generate_exp1.py --game cave_treasures --instance 1 --epochs 2000 --solution True
+python ./rumi/generate_exp1.py --game cave_treasures --instance 1 --epochs 2000 --solution True
+python ./conditional/generate_exp1.py --game cave_treasures --instance 1 --epochs 1000 --solution True
 
 # exp2 
-python ./vanilla/generate_exp2.py --game mario --instance 1 --epochs 500 --solution True --cond 1
-python ./vanilla/generate_exp2.py --game mario --instance 1 --epochs 500 --solution True --cond 2
-python ./vanilla/generate_exp2.py --game mario --instance 1 --epochs 500 --solution True --cond 3
+python ./vanilla/generate_exp2.py --game mario --instance 1 --epochs 3000 --solution True --cond 1
+python ./vanilla/generate_exp2.py --game mario --instance 1 --epochs 3000 --solution True --cond 2
+python ./vanilla/generate_exp2.py --game mario --instance 1 --epochs 3000 --solution True --cond 3
 
-python ./rumi/generate_exp2.py --game mario --instance 1 --epochs 500 --solution True --cond 1
-python ./rumi/generate_exp2.py --game mario --instance 1 --epochs 500 --solution True --cond 2
-python ./rumi/generate_exp2.py --game mario --instance 1 --epochs 500 --solution True --cond 3
+python ./rumi/generate_exp2.py --game mario --instance 1 --epochs 3000 --solution True --cond 1
+python ./rumi/generate_exp2.py --game mario --instance 1 --epochs 3000 --solution True --cond 2
+python ./rumi/generate_exp2.py --game mario --instance 1 --epochs 3000 --solution True --cond 3
 
-python ./conditional/generate_exp2.py --game mario --instance 1 --epochs 500 --solution True --cond 1
+python ./conditional/generate_exp2.py --game mario --instance 1 --epochs 1000 --solution True --cond 1
 python ./conditional/generate_exp2.py --game mario --instance 1 --epochs 500 --solution True --cond 2
-python ./conditional/generate_exp2.py --game mario --instance 1 --epochs 500 --solution True --cond 3
+python ./conditional/generate_exp2.py --game mario --instance 1 --epochs 1000 --solution True --cond 3
 
-python ./vanilla/generate_exp2.py --game cave_treasures --instance 1 --epochs 500 --solution True --cond 1
-python ./vanilla/generate_exp2.py --game cave_treasures --instance 1 --epochs 500 --solution True --cond 2
-python ./vanilla/generate_exp2.py --game cave_treasures --instance 1 --epochs 500 --solution True --cond 3
+python ./vanilla/generate_exp2.py --game cave_treasures --instance 1 --epochs 3000 --solution True --cond 1
+python ./vanilla/generate_exp2.py --game cave_treasures --instance 1 --epochs 3000 --solution True --cond 2
+python ./vanilla/generate_exp2.py --game cave_treasures --instance 1 --epochs 3000 --solution True --cond 3
 
-python ./rumi/generate_exp2.py --game cave_treasures --instance 1 --epochs 500 --solution True --cond 1
-python ./rumi/generate_exp2.py --game cave_treasures --instance 1 --epochs 500 --solution True --cond 2
-python ./rumi/generate_exp2.py --game cave_treasures --instance 1 --epochs 500 --solution True --cond 3
+python ./rumi/generate_exp2.py --game cave_treasures --instance 1 --epochs 3000 --solution True --cond 1
+python ./rumi/generate_exp2.py --game cave_treasures --instance 1 --epochs 3000 --solution True --cond 2
+python ./rumi/generate_exp2.py --game cave_treasures --instance 1 --epochs 3000 --solution True --cond 3
 
-python ./conditional/generate_exp2.py --game cave_treasures --instance 1 --epochs 500 --solution True --cond 1
-python ./conditional/generate_exp2.py --game cave_treasures --instance 1 --epochs 500 --solution True --cond 2
-python ./conditional/generate_exp2.py --game cave_treasures --instance 1 --epochs 500 --solution True --cond 3
+python ./conditional/generate_exp2.py --game cave_treasures --instance 1 --epochs 1000 --solution True --cond 1
+python ./conditional/generate_exp2.py --game cave_treasures --instance 1 --epochs 1000 --solution True --cond 2
+python ./conditional/generate_exp2.py --game cave_treasures --instance 1 --epochs 1000 --solution True --cond 3
+
+# exp3
+python ./rumi/generate_exp3.py --game mario --instance 1 --epochs 3000 --solution True --cond 1
+python ./rumi/generate_exp3.py --game mario --instance 1 --epochs 3000 --solution True --cond 2
+python ./rumi/generate_exp3.py --game mario --instance 1 --epochs 3000 --solution True --cond 3
+
+python ./conditional/generate_exp3.py --game mario --instance 1 --epochs 2000 --solution True --cond 1
+python ./conditional/generate_exp3.py --game mario --instance 1 --epochs 2000 --solution True --cond 2
+python ./conditional/generate_exp3.py --game mario --instance 1 --epochs 2000 --solution True --cond 3
+
+python ./rumi/generate_exp3.py --game cave_treasures --instance 1 --epochs 3000 --solution True --cond 1
+python ./rumi/generate_exp3.py --game cave_treasures --instance 1 --epochs 3000 --solution True --cond 2
+python ./rumi/generate_exp3.py --game cave_treasures --instance 1 --epochs 3000 --solution True --cond 3
+
+python ./conditional/generate_exp3.py --game cave_treasures --instance 1 --epochs 2000 --solution True --cond 1
+python ./conditional/generate_exp3.py --game cave_treasures --instance 1 --epochs 2000 --solution True --cond 2
+python ./conditional/generate_exp3.py --game cave_treasures --instance 1 --epochs 2000 --solution True --cond 3
 
 
 # python ./vanilla/generate_exp1.py --game platform --instance 1 --epochs 5000 --solution True
